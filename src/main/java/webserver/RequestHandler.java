33cf1bb1e4e9bed926d40b9836b941fd6ac3c813
lalwr <lalwrj@gmail.com>
2018-07-13 01:02:53 +0900

    3.4.3.1 요구사항 1 - index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,9 +21,24 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            //TODO 3.4.3.1 요구사항1 indexl.html 응답하기
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            log.debug("request line : {}", line);
+
+            String[] tokens = line.split(" ");
+
+            byte[] body = Files.readAllBytes(new File("./webapp" + tokens[1]).toPath());
+
+            if(line == null) return;
+
+            while(!"".equals(line)){
+                line = br.readLine();
+                log.debug("header : {}", line);
+            }
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World JWP".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
