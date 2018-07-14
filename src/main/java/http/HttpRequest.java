4a4e957b89f22185f4759f5bca0bfd1b4b7d6633
lalwr <lalwrj@gmail.com>
2018-07-15 02:50:09 +0900

    5.1.2.1 요청 데이터를 처리하는 로직을 별도의 클래스로 분리한다(HttpRequest) 리팩토링

--- src/main/java/http/HttpRequest.java
+++ src/main/java/http/HttpRequest.java
@@ -13,76 +13,61 @@ import util.IOUtils;
 public class HttpRequest {
     private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
 
-    Map<String, String> requestMap = new HashMap<>();
+    private Map<String, String> headers = new HashMap<String, String>();
+    private Map<String, String> params = new HashMap<String, String>();
+    private RequestLine requestLine;
 
     public HttpRequest(InputStream in){
         try {
-            BufferedReader br = null;
-            br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
             String line = br.readLine();
-
             if(line == null) return;
             log.debug("request line : {}", line);
-            String[] tokens = line.split(" ");
 
-            requestMap.put("method", tokens[0]);
-            String params = "";
-            String path = tokens[1];
-            if(tokens[1].contains("?")){
-                int index = path.indexOf("?");
-                path = path.substring(0, index);
-                params = tokens[1].substring(index+1);
-            }
-            requestMap.put("path", path);
+            requestLine = new RequestLine(line);
 
-            int contentLength = 0;
             while(!"".equals(line = br.readLine()) ){
-                log.debug("request line : {}", line);
-                tokens = line.split(" ");
-                requestMap.put(tokens[0].replace(":", ""), tokens[1]);
-
-                if(line.startsWith("Content-Length:")){
-                    contentLength = Integer.parseInt(tokens[1]);
-                }
+                log.debug("header : {}", line);
+                String[] tokens = line.split(":");
+                headers.put(tokens[0].trim(), tokens[1].trim());
             }
 
-            if("GET".equals(getMethod())){
-                Map<String, String> param = HttpRequestUtils.parseQueryString(params);
-                Iterator<String> keys = param.keySet().iterator();
-                while (keys.hasNext()){
-                    String key = keys.next();
-                    requestMap.put(key, param.get(key));
-                }
-            }else if("POST".equals(getMethod())){
-                String body = IOUtils.readData(br, contentLength);
-                Map<String, String> param = HttpRequestUtils.parseQueryString(body);
-                Iterator<String> keys = param.keySet().iterator();
-                while (keys.hasNext()){
-                    String key = keys.next();
-                    requestMap.put(key, param.get(key));
-                }
+            if(HttpMethod.POST.equals(getMethod())){
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            }else{
+                params = requestLine.getParams();
             }
 
-        } catch (IOException e) {
-            log.error(e.getMessage());
+        } catch (IOException io) {
+            log.error(io.getMessage());
         }
 
     }
 
-    public String getMethod(){
-        return requestMap.get("method");
+    public HttpMethod getMethod(){
+        return requestLine.getMethod();
     }
 
     public String getPath(){
-        return requestMap.get("path");
+        return requestLine.getPath();
     }
 
-    public String getHeader(String headerName){
-        return requestMap.get(headerName);
+    public String getParameter(String name){
+        return params.get(name);
     }
 
-    public String getParameter(String parameterName){
-        return requestMap.get(parameterName);
+    public String getHeader(String name){
+        return headers.get(name);
     }
+
+    public boolean isLogin(){
+        Map<String, String> cookies = HttpRequestUtils.parseCookies(getHeader("Cookie"));
+        String value = cookies.get("logined");
+        if(value == null){
+            return false;
+        }
+        return Boolean.parseBoolean(value);
+    }
+
 }
